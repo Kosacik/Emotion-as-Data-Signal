bc7bfdc9bace725326c607b388faabc606bc076e
Kosacik <radim@niknak.cz>
2026-06-30 22:11:27 +0200

    opravy

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const header = document.querySelector(".site-header");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 80) {
-    header.classList.add("is-scrolled");
-  } else {
-    header.classList.remove("is-scrolled");
-  }
+
+    if(window.scrollY > 140){
+
+        header.classList.add("is-scrolled");
+
+    }else{
+
+        header.classList.remove("is-scrolled");
+
+    }
+
 });
